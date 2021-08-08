f4f72e2c646266146d42b98d29d5655812889a2d
HPROGRAM <wjs2096@gmail.com>
2021-08-08 16:53:56 +0900

    수정: 설정파일 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { BoardsModule } from './boards/boards.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3000,
+      username: 'root',
+      password: '1234',
+      database: 'community_data',
+      entities: [], // 사용할 entity의 클래스명을 넣어둔다.
+      synchronize: true, // false로 해두는 게 안전하다.
+    }),
+    BoardsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
