13c76502534dbeae65d852727a81b6bb79091613
HPROGRAM <wjs2096@gmail.com>
2021-08-09 19:39:09 +0900

    업데이트: LIKES 테이블 사용 가능 하도록 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
-import { Board } from 'src/boards/board.entity';
+import { User } from '../entities/user.entity';
+import { Board } from 'src/entities/board.entity';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Board]),
     JwtModule.register({
       secret: '10',
       signOptions: { expiresIn: '500s' },
